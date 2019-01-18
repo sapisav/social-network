9fa09ee45b2b62d9c7fab1d3c59dee01f5cab757
sapisav <sapisav@gmail.com>
2019-01-18 15:53:21 +0200

    added login verification with DB

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,9 +1,41 @@
 /*eslint-env browser*/
 
-/* ########## index.html ########## */
+let $userName = document.querySelector("#userName");
+let $pw = document.querySelector("#pw");
+let $alert = document.querySelector(".alert");
 // redirect to feed after login button is clicked	
 document.getElementById("loginBTN").onclick = function () {
-	location.href = "feed.html";
+	validateUsernameAndPw();
 };
+// alert user when username or pw wrong for 3seconds
+function showAlert(bool){
+	if(bool){
+		$alert.style.display = 'block';
+		setTimeout(showAlert,3000,false);
+	}else $alert.style.display = 'none';
+
+}
+
+
+
+async function validateUsernameAndPw(){
+	let user = await fetch(`http://localhost:3000/users?userName=${$userName.value}`)
+        .then(function (response) {
+            return response.json();
+        })
+
+    
+    if(user[0] == undefined){
+		showAlert(true);
+		
+	}
+	else if(user[0].password != $pw.value){
+		showAlert(true);
+	}
+	else {
+		localStorage.setItem("logged-in-as", `${user[0].userName}`);
+		location.href = "feed.html";
+	}
+}
 
 
