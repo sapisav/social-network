8cbb186a505b32984da588a089d2452a13a7759f
sapisav <sapisav@gmail.com>
2019-01-18 18:09:22 +0200

    some db tests, only for me

--- backend/tests.js
+++ backend/tests.js
@@ -9,23 +9,29 @@ async function f(o) {
     
     console.log(o);
     ////////////////// POST /////////////////
-    // var url = 'http://localhost:3000/posts';
-    // var data = {};
+    var url = 'http://localhost:3000/users';
+    let newUser = new User("a@a.com", "123123");
     // data.title = "safgfgfgfgfgfgfsa";
     
-    // fetch(url, {
-    //     method: 'POST', // or 'PUT'
-    //     body: JSON.stringify(data), // data can be `string` or {object}!
-    //     headers: {
-    //         'Content-Type': 'application/json'
-    //     }
-    // }).then(res => res.json())
-    // .then(response => console.log('Success:', JSON.stringify(response)))
-    // .catch(error => console.error('Error:', error));
+    fetch(url, {
+        method: 'POST', // or 'PUT'
+        body: JSON.stringify(newUser), // data can be `string` or {object}!
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(res => res.json())
+    .then(response => console.log('Success:', JSON.stringify(response)))
+    .catch(error => console.error('Error:', error));
 
 
 }
 f(obj);
+
+//create user obj
+function User(userName, pw) {
+	this.userName = userName;
+	this.password = pw;
+}
 
 
 
