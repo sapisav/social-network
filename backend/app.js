9c6a4f5a20e56d44b38a1794f42bc525f7a22ea4
sapisav <sapisav@gmail.com>
2019-02-16 10:16:32 +0200

    backend first commit

---
+++ backend/app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const commonMW = require("./middlewares/default-mw"); //default middlewares
+const { sessionCounterMW, sessionMgmtMW } = require("./middlewares/sessions");
+const { authBySession } = require("./middlewares/auth-mw"); //authentication middlewares
+const path = require("path");
+
+const app = express();
+app.use(commonMW);
+app.use(express.static(path.join(__dirname, process.argv[2] || "./static")));
+app.use([sessionMgmtMW, sessionCounterMW]);
+
+const wsRef = require("express-ws")(app); //give app the ability to use WS
+app.use("/ws", authBySession, require("./routes/websockets"));
+
+const homepageRouter = require("./routes/homepage");
+const usersRouter = require("./routes/users");
+const meRouter = require("./routes/me");
+app.use("/", homepageRouter);
+app.use("/users", authBySession, usersRouter);
+app.use("/me", authBySession, meRouter);
+
+app.listen(3000);
+
+module.exports = app;
